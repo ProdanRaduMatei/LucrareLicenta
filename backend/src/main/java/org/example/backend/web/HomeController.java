539206a1103e19f3d71c94f18fc9e5f8e41ea01d
matei <mateiprodan1@gmail.com>
2025-02-11 08:57:32 +0200

    OAuth2 sign in implemented for Google and Github

---
+++ backend/src/main/java/org/example/backend/web/HomeController.java
@@ -0,0 +1,17 @@
+package org.example.backend.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, Secured!";
+    }
+}
